07f868fa63536c27714c1154a36aae69741d0530
silvansky <v.silvansky@gmail.com>
2012-04-10 17:22:29 +0400

    initial commit

---
+++ test_python.py
@@ -0,0 +1,26 @@
+"""Test python module"""
+
+import sys;
+
+def func1():
+    print("Hello!");
+
+def func2(a):
+    print(a+2);
+    return a-2;
+
+func1();
+
+lst = [5, 6, 3, 6];
+print("lst: ", lst);
+
+for i in lst:
+    print(func2(i));
+
+a, b = 5, 1;
+
+a, b = b, a + b;
+
+print(a, b);
+
+print(sys.argv);
